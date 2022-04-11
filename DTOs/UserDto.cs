cdb3131f1683d3ea69462f9233c0d6e9ec995c59
DylanDabrowski <dylandabrowski@gmail.com>
2022-04-10 23:59:21 -0400

    User login and register working with password salt and hash.

---
+++ DTOs/UserDto.cs
@@ -0,0 +1,8 @@
+namespace WatercoolerApi.DTOs
+{
+    public class UserDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
