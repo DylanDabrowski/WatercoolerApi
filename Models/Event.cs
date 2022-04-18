cd8b76804af2fd66186815d262ad0f498db0d0bd
DylanDabrowski <dylandabrowski@gmail.com>
2022-04-18 13:07:33 -0400

    event model made

---
+++ Models/Event.cs
@@ -0,0 +1,10 @@
+namespace WatercoolerApi.Models
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ImageUri { get; set; } = string.Empty;
+    }
+}
